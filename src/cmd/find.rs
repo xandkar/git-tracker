3504a2e0f2c66d8b8afc7f7cfcc57047c4685c5c
Siraaj Khandkar <siraaj@khandkar.net>
2024-08-16 12:12:31 -0400

    Fetch remotes

--- src/cmd/find.rs
+++ src/cmd/find.rs
@@ -1,9 +1,10 @@
-use std::{collections::HashSet, path::PathBuf};
+use std::{collections::HashSet, path::PathBuf, sync::Arc};
 
 use anyhow::Context;
+use dashmap::DashSet;
 use futures::{stream, StreamExt};
 
-use crate::git;
+use crate::{git, os};
 
 #[derive(clap::Args, Debug, Clone)]
 pub struct Cmd {
@@ -14,37 +15,67 @@ pub struct Cmd {
     // TODO These should actualy be regexp patterns to filter candidate paths.
     /// Ignore this path when searching for repos.
     #[clap(short, long)]
-    ignore: Vec<PathBuf>,
+    ignore_paths: Vec<PathBuf>,
 
     /// Local paths to explore for potential git repos.
-    paths: Vec<PathBuf>,
+    search_paths: Vec<PathBuf>,
 }
 
 impl Cmd {
     pub async fn run(&self) -> anyhow::Result<()> {
-        let ignore: HashSet<PathBuf> = self.ignore.iter().cloned().collect();
-        let mut paths = Vec::new();
-        for path in &self.paths {
+        let ignore_paths: HashSet<PathBuf> =
+            self.ignore_paths.iter().cloned().collect();
+        let mut search_paths = Vec::new();
+        for path in &self.search_paths {
             let path = path
                 .canonicalize()
                 .context(format!("Invalid local path={path:?}"))?;
-            paths.push(path);
+            search_paths.push(path);
         }
-
-        stream::iter(paths)
+        let urls: Arc<DashSet<String>> = Arc::new(DashSet::new());
+        let host = os::hostname().await?;
+        stream::iter(search_paths)
             .flat_map(|path| {
-                crate::fs::find_dirs(&path, ".git", self.follow, &ignore)
+                crate::fs::find_dirs(
+                    &path,
+                    ".git",
+                    self.follow,
+                    &ignore_paths,
+                )
             })
-            .filter(|path| git::local_is_repo(path.clone()))
-            .filter_map(|path| async move {
-                let res = git::Local::read(&path).await;
-                if let Err(error) = &res {
-                    tracing::error!(?path, ?error, "Failed to read repo.");
+            .filter(|path| git::is_repo(path.clone()))
+            .map(|path| git::Link::Fs { dir: path })
+            .filter_map(|link| {
+                let host = host.clone();
+                async move { git::view(&host, &link).await.ok() }
+            })
+            .for_each_concurrent(None, {
+                |view| {
+                    let urls = Arc::clone(&urls);
+                    async move {
+                        println!("{:#?}", &view);
+                        for url in view
+                            .repo
+                            .iter()
+                            .flat_map(|repo| repo.remotes.values())
+                        {
+                            urls.insert(url.to_string());
+                        }
+                    }
                 }
-                res.ok()
             })
-            .for_each_concurrent(None, |repo| async move {
-                println!("{repo:#?}");
+            .await;
+        dbg!(&urls);
+        stream::iter(urls.iter())
+            .map(|url| git::Link::Net {
+                url: url.to_string(),
+            })
+            .filter_map(|link| {
+                let host = host.clone();
+                async move { git::view(&host, &link).await.ok() }
+            })
+            .for_each_concurrent(None, |view| async move {
+                println!("{:#?}", &view);
             })
             .await;
         Ok(())
--- src/git.rs
+++ src/git.rs
@@ -10,37 +10,65 @@ use anyhow::{anyhow, bail};
 use crate::os;
 
 #[derive(Debug)]
+pub struct View {
+    pub host: String,
+    pub link: Link,
+    pub repo: Option<Repo>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Link {
+    Fs { dir: PathBuf },
+    Net { url: String },
+}
+
+#[derive(Debug)]
 pub struct Branch {
     pub roots: HashSet<String>,
     pub leaf: String,
 }
 
 #[derive(Debug)]
-pub struct Local {
-    pub hostname: String,
-    pub path: PathBuf,
-    pub is_bare: bool, // TODO Does it really matter for us if a repo is bare?
+pub struct Repo {
     pub description: Option<String>,
+    pub remotes: HashMap<String, String>,
     pub branches: HashMap<String, Branch>,
-    pub remotes: HashMap<String, String>, // TODO Parse/validate URL/addr.
 }
 
-impl Local {
+impl Repo {
     #[tracing::instrument]
-    pub async fn read<P>(dir: P) -> anyhow::Result<Self>
+    pub async fn read_from_link(link: &Link) -> anyhow::Result<Self> {
+        let result = match link {
+            Link::Fs { dir } => Self::read_from_fs(dir).await,
+            Link::Net { url } => Self::read_from_url(url).await,
+        };
+        if let Err(error) = &result {
+            tracing::error!(?link, ?error, "Failed to read repo.");
+        }
+        result
+    }
+
+    #[tracing::instrument]
+    pub async fn read_from_fs<P>(dir: P) -> anyhow::Result<Self>
     where
         P: AsRef<Path> + std::fmt::Debug,
     {
         let dir = dir.as_ref();
         let selph = Self {
-            is_bare: local_is_bare(dir).await?,
-            description: local_description(dir).await?,
-            branches: local_branches(dir).await?,
-            remotes: local_remote_refs(dir).await?,
-            path: dir.to_path_buf(),
-            hostname: os::hostname().await?,
+            description: description(dir).await?,
+            branches: branches(dir).await?,
+            remotes: remote_refs(dir).await?,
         };
         Ok(selph)
+    }
+
+    #[tracing::instrument]
+    pub async fn read_from_url(url: &str) -> anyhow::Result<Self> {
+        let dir = tempfile::tempdir()?;
+        let dir = dir.path();
+        tracing::debug!(?url, ?dir, "Cloning");
+        clone_bare(url, dir).await?;
+        Self::read_from_fs(dir).await
     }
 }
 
@@ -85,7 +113,6 @@ impl FromStr for RemoteRef {
             .next()
             .map(|str| str.to_string())
             .ok_or_else(|| anyhow!("Remote line is empty: {s:?}"))?;
-        // TODO Parse/validate addr/URL?
         let addr = fields
             .next()
             .map(|str| str.to_string())
@@ -94,7 +121,16 @@ impl FromStr for RemoteRef {
     }
 }
 
-pub async fn local_is_repo<P: AsRef<Path>>(dir: P) -> bool {
+pub async fn view(host: &str, link: &Link) -> anyhow::Result<View> {
+    let view = View {
+        host: host.to_string(),
+        link: link.clone(),
+        repo: Repo::read_from_link(link).await.ok(),
+    };
+    Ok(view)
+}
+
+pub async fn is_repo<P: AsRef<Path>>(dir: P) -> bool {
     let git_dir = format!("--git-dir={}", dir.as_ref().to_string_lossy());
     os::cmd("git", &[&git_dir, "log", "--format=", "-1"])
         .await
@@ -102,21 +138,19 @@ pub async fn local_is_repo<P: AsRef<Path>>(dir: P) -> bool {
 }
 
 #[tracing::instrument(skip_all)]
-async fn local_branches(
-    dir: &Path,
-) -> anyhow::Result<HashMap<String, Branch>> {
+async fn branches(dir: &Path) -> anyhow::Result<HashMap<String, Branch>> {
     let mut branches = HashMap::new();
     // XXX Looking up roots for all refs, rather than just branches, takes a
     //     long time for repos with many tags and long history.
-    for (name, leaf) in local_branch_leaves(dir).await? {
-        let roots = local_branch_roots(dir, &leaf).await?;
+    for (name, leaf) in branch_leaves(dir).await? {
+        let roots = branch_roots(dir, &leaf).await?;
         branches.insert(name, Branch { roots, leaf });
     }
     Ok(branches)
 }
 
 #[tracing::instrument(skip_all)]
-async fn local_branch_leaves(
+async fn branch_leaves(
     dir: &Path,
 ) -> anyhow::Result<HashMap<String, String>> {
     let git_dir = format!("--git-dir={}", dir.to_string_lossy());
@@ -135,9 +169,37 @@ async fn local_branch_leaves(
 }
 
 #[tracing::instrument(skip_all)]
-async fn local_remote_refs(
-    dir: &Path,
-) -> anyhow::Result<HashMap<String, String>> {
+pub async fn clone_bare(
+    from_addr: &str,
+    to_dir: &Path,
+) -> anyhow::Result<()> {
+    let to_dir = to_dir.to_string_lossy().to_string();
+    // Q: How to prevent git from prompting for credentials and fail instead?
+    // A: https://serverfault.com/a/1054253/156830
+    let env = HashMap::from([
+        ("GIT_SSH_COMMAND", "ssh -oBatchMode=yes"),
+        ("GIT_TERMINAL_PROMPT", "0"),
+        ("GIT_ASKPASS", "echo"),
+        ("SSH_ASKPASS", "echo"),
+        ("GCM_INTERACTIVE", "never"),
+    ]);
+    let exe = "git";
+    let args = &["clone", "--bare", from_addr, &to_dir];
+    let out = tokio::process::Command::new(exe)
+        .args(args)
+        .envs(&env)
+        .output()
+        .await?;
+    out.status.success().then_some(()).ok_or_else(|| {
+        anyhow!(
+            "Failed to execute command: exe={exe:?} args={args:?} env={env:?} err={:?}",
+            String::from_utf8_lossy(&out.stderr[..])
+        )
+    })
+}
+
+#[tracing::instrument(skip_all)]
+async fn remote_refs(dir: &Path) -> anyhow::Result<HashMap<String, String>> {
     let git_dir = format!("--git-dir={}", dir.to_string_lossy());
     let mut remotes = HashMap::new();
     for line_result in
@@ -151,7 +213,7 @@ async fn local_remote_refs(
 }
 
 #[tracing::instrument(skip(dir))]
-pub async fn local_branch_roots(
+pub async fn branch_roots(
     dir: &Path,
     leaf_hash: &str,
 ) -> anyhow::Result<HashSet<String>> {
@@ -170,7 +232,7 @@ pub async fn local_branch_roots(
 }
 
 #[tracing::instrument(skip_all)]
-pub async fn local_is_bare(dir: &Path) -> anyhow::Result<bool> {
+pub async fn is_bare(dir: &Path) -> anyhow::Result<bool> {
     let git_dir = format!("--git-dir={}", dir.to_string_lossy());
     let out =
         os::cmd("git", &[&git_dir, "rev-parse", "--is-bare-repository"])
@@ -181,7 +243,7 @@ pub async fn local_is_bare(dir: &Path) -> anyhow::Result<bool> {
 }
 
 #[tracing::instrument(skip_all)]
-async fn local_description(dir: &Path) -> io::Result<Option<String>> {
+async fn description(dir: &Path) -> io::Result<Option<String>> {
     tokio::fs::read_to_string(dir.join("description"))
         .await
         .map(|s| (!s.starts_with("Unnamed repository;")).then_some(s))
